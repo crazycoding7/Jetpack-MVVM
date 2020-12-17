a8a255a1aad1871d69ffd3a99a26d4297319cfac
qishuai <644172593@qq.com>
2020-12-17 15:29:02 +0800

    曲线地图测试demo

--- app/src/main/java/com/alex/androidjetpack/ui/maplinedemo/CustomView/PathView.java
+++ app/src/main/java/com/alex/androidjetpack/ui/maplinedemo/CustomView/PathView.java
@@ -8,6 +8,8 @@ import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.ArrayList;
+
 /**
  * 时 间: 2016/11/8 0008
  * 作 者: 郑亮
@@ -18,10 +20,28 @@ public class PathView extends View {
 
     private Paint paint;
 
+    ArrayList<int[]> points = new ArrayList<>();
 
     public PathView(Context context, AttributeSet attrs) {
         super(context, attrs);
         initView();
+
+        points.add(new int[]{430, 230});
+        points.add(new int[]{460, 265});
+        points.add(new int[]{480, 280});
+        points.add(new int[]{490, 285});
+        points.add(new int[]{500, 300});
+        points.add(new int[]{495, 315});
+        points.add(new int[]{492, 330});
+        points.add(new int[]{470, 370});
+        points.add(new int[]{475, 380});
+        points.add(new int[]{480, 430});
+
+        points.add(new int[]{500, 460});
+
+        points.add(new int[]{520, 485});
+//        points.add(new int[]{400, 580});
+//        points.add(new int[]{460, 680});
     }
 
     private void initView() {
@@ -35,25 +55,62 @@ public class PathView extends View {
         //设置颜色
         paint.setColor(Color.GREEN);
         //设置画笔宽度
-        paint.setStrokeWidth(3);
+        paint.setStrokeWidth(10);
     }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
+
         super.onDraw(canvas);
         Path path = new Path();
         path.moveTo(60,60);
-        path.lineTo(460,460);
 
-        boolean xChange = false;
-        for (int i =20;i< 1000;i=i+20){
-            int x = xChange? 460:600;
-            xChange = !xChange;
+        int[] pre = null;
+        for(int i = 0 ;i < points.size();i++){
+            int[] point = points.get(i);
+            if(i == 0){
+                path.moveTo(point[0],point[1]);
+                pre = point;
+                continue;
+            }
 
-            path.lineTo(x,460 + i*2);
+            path.lineTo(point[0],point[1]);
+
+//            path.quadTo((pre[0]+point[0])/2, point[1], point[0],point[1]);
+
+            pre = point;
         }
 
-//        path.quadTo(660, 260, 860, 460); //订单
+//        path.lineTo(460,460); // 760 860
+//
+//        path.cubicTo(610,160,610,860,760,860);
+
+
+
+        //path.lineTo(440,490);
+        //path.lineTo(450,530);
+        //path.lineTo(400,580);
+        //path.lineTo(460,680);
+
+
+
+
+//        path.lineTo(460,460);
+//        path.lineTo(460,460);
+//        path.lineTo(460,460);
+//        path.lineTo(460,460);
+//        boolean xChange = false;
+//        for (int i =20;i< 1000;i=i+20){
+//            int x = xChange? 460:600;
+//            xChange = !xChange;
+//
+//            path.lineTo(x,460 + i*2);
+//            //path.quadTo(660, 400+i*2, x,460 + i*2);
+//
+//        }
+
+       // path.quadTo(660, 260, 860, 460); //订单
 //        path.cubicTo(160,660,960,1060,260,1260);
         canvas.drawPath(path,paint);
     }
